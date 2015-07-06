e06758d98b28f6c3e8f42e87455c5e8cd965ede9
Rob Coward <rob@jive-videos.net>
2015-07-06 23:09:54 +0100

    Installation of windows opsview client - this is still work in progress.

---
+++ attributes/setup_windows_agent.rb
@@ -0,0 +1,5 @@
+
+default['opsview']['agent']['x64']['url'] = 'https://s3.amazonaws.com/opsview-agents/Windows/Opsview_Windows_Agent_x64.msi'
+default['opsview']['agent']['Win32']['url'] = 'https://s3.amazonaws.com/opsview-agents/Windows/Opsview_Windows_Agent_Win32.msi'
+default['opsview']['agent']['windows_conf_dir'] = 'C:\Program Files\opsview\NSClient++'
+default['opsview']['agent']['manage_opsview_config'] = true
