96bfde5e3fb61071f95d031d197524016d3e56db
Rob Coward <rob@jive-videos.net>
2015-07-07 12:41:46 +0100

    Added win2012 platform to test-kitchen - assumes you have already imported a box file called win2012. Updated download urls for windows agents.

--- recipes/setup_windows_agent.rb
+++ recipes/setup_windows_agent.rb
@@ -24,7 +24,7 @@ windows_package "Opsview NSClient++ Windows Agent (#{arch})" do
 	action :install
 end
 
-template "#{node['opsview']['agent']['windows_conf_dir']}/NSC.ini" do
+template ::File.join(node['opsview']['agent']['windows_conf_dir'], 'NSC.ini') do
   source 'NSC.ini.erb'
   notifies :restart, 'service[NSClientpp]'
   action node['opsview']['agent']['manage_ncslient_config'] ? :create : :create_if_missing
